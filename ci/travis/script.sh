a47e2b6f51d909a7f6efe376c7e4fe0ed70bfbbd
Nikita Uvarov <dragonicthebest@gmail.com>
2018-09-16 02:21:07 +0300

    More silent apt-get

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -8,8 +8,8 @@ function install_cuda_linux()
 {
     wget -q https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb -O cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb
     sudo dpkg -i cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb
-    sudo apt-get update
-    sudo apt-get install cuda    
+    sudo apt-get update -qq -o=Dpkg::Use-Pty=0
+    sudo apt-get install -qq -o=Dpkg::Use-Pty=0 cuda    
 }
 
 
